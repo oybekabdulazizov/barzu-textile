fe7bbbd7ad6a4b65d82cbd1190227e21eea17374
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-02-25 15:19:44 +0100

    make carousel content responsive

--- components/shared/HeroCarousel.tsx
+++ components/shared/HeroCarousel.tsx
@@ -29,13 +29,13 @@ const HeroCarousel: React.FC = ({}) => {
     >
       <div className='absolute z-10 bg-black bg-opacity-60 w-full h-full flex items-center'>
         <div className='wrapper'>
-          <h1 className='text-slate-200 text-[80px] 2xl:text-[100px] font-bold'>
+          <h1 className='text-slate-200 text-[40px] md:text-[60px] lg:text-[80px] 2xl:text-[100px] font-bold'>
             Barzu
           </h1>
-          <p className='text-slate-200 text-[34px] font-medium tracking-wide'>
+          <p className='text-slate-200 text-[20px] md:text-[26px] lg:text-[34px] font-medium tracking-wide'>
             Discover our superior knitwear for unmatched quality and style!
           </p>
-          <p className='text-slate-200 text-[20px] font-medium pt-2 tracking-wide'>
+          <p className='text-slate-200 text-[14px] md:text-[20px] font-medium pt-2 tracking-wide'>
             From production to doorstep, we've got you covered with seamless
             service, packaging, and delivery solutions!
           </p>
@@ -44,13 +44,16 @@ const HeroCarousel: React.FC = ({}) => {
       <CarouselContent>
         {heroCarouselImages.map(({ src, alt }, index) => (
           <CarouselItem key={index} className='p-0'>
-            <div>
-              <Card className='border-none p-0'>
-                <CardContent className='flex items-center h-[610px] 2xl:h-[900px] p-0'>
-                  <Image className='w-full' src={src} alt={alt} />
-                </CardContent>
-              </Card>
-            </div>
+            <Card className='border-none p-0'>
+              <CardContent className='relative flex items-center min-h-[600px] md:max-h-[900px] p-0'>
+                <Image
+                  className='min-h-[600px] object-cover object-center'
+                  src={src}
+                  alt={alt}
+                  priority
+                />
+              </CardContent>
+            </Card>
           </CarouselItem>
         ))}
       </CarouselContent>
