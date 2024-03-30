186d4f26136cf530bbeb37864a169de5991d1a0b
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-03-30 23:54:32 +0100

    replace Inter font with DM_Sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { DM_Sans } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/shared/Header';
 import Footer from '@/components/shared/Footer';
 
-const inter = Inter({ subsets: ['latin'] });
+const dmSans = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${inter.className} min-h-screen flex flex-col justify-between`}
+        className={`${dmSans.className} min-h-screen flex flex-col justify-between`}
       >
         <Header />
         {children}
