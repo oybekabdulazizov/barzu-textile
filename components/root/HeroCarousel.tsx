e7d2b9e7e3d2935cfc1c4e34ff7fdf24d8570a04
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-04-01 15:01:37 +0200

    move carousel cover inside carousel

--- components/root/HeroCarousel.tsx
+++ components/root/HeroCarousel.tsx
@@ -20,44 +20,47 @@ const HeroCarousel: React.FC = ({}) => {
   );
 
   return (
-    <Carousel
-      plugins={[plugin.current]}
-      className='w-full'
-      opts={{
-        loop: true,
-      }}
-    >
-      <div className='absolute z-10 bg-black bg-opacity-60 w-full h-full flex items-center'>
-        <div className='wrapper flex flex-col gap-2'>
-          <h1 className='text-slate-200 text-4xl md:text-6xl lgt:ext-7xl 2xl:text-8xl font-bold'>
-            Barzu
-          </h1>
-          <p className='text-slate-200 text-xl md:text-2xl lg:text-4xl font-medium tracking-wide'>
-            Discover our superior knitwear for unmatched quality and style!
-          </p>
-          <p className='text-slate-200 text-sm md:text-xl font-medium pt-2 tracking-wide'>
-            From production to doorstep, we've got you covered with seamless
-            service, packaging, and delivery solutions!
-          </p>
-        </div>
-      </div>
-      <CarouselContent>
-        {heroCarouselImages.map(({ src, alt }, index) => (
-          <CarouselItem key={index} className='p-0'>
-            <Card className='border-none p-0'>
-              <CardContent className='relative flex items-center min-h-[600px] min-[3350px]:min-h-[1200px] min-[3350px]:justify-center md:max-h-[800px] p-0'>
-                <Image
-                  className='min-h-[600px] min-[3350px]:min-h-[1200px] object-cover object-center'
-                  src={src}
-                  alt={alt}
-                  priority
-                />
-              </CardContent>
-            </Card>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-    </Carousel>
+    <div>
+      <Carousel
+        plugins={[plugin.current]}
+        className='w-full'
+        opts={{
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {heroCarouselImages.map(({ src, alt }, index) => (
+            <CarouselItem key={index} className='p-0'>
+              <Card className='border-none'>
+                <CardContent className='relative flex items-center min-[3350px]:justify-center min-h-[600px] min-[3350px]:min-h-[1200px] md:max-h-[800px] p-0'>
+                  <div className='absolute bg-black bg-opacity-60 w-full h-full flex items-center'>
+                    <div className='wrapper flex flex-col gap-2'>
+                      <h1 className='text-slate-200 text-4xl md:text-6xl lgt:ext-7xl 2xl:text-8xl font-bold'>
+                        Barzu
+                      </h1>
+                      <p className='text-slate-200 text-xl md:text-2xl lg:text-4xl font-medium tracking-wide'>
+                        Discover our superior knitwear for unmatched quality and
+                        style!
+                      </p>
+                      <p className='text-slate-200 text-sm md:text-xl font-medium pt-2 tracking-wide'>
+                        From production to doorstep, we've got you covered with
+                        seamless service, packaging, and delivery solutions!
+                      </p>
+                    </div>
+                  </div>
+                  <Image
+                    className='full object-cover object-center'
+                    src={src}
+                    alt={alt}
+                    priority
+                  />
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
   );
 };
 
