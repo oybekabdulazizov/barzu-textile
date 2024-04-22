6eeb8d306d01e4114bc37c40c39ba91e665a09fe
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-04-22 23:02:22 +0200

    update the contents and styles of boy's products page

--- components/shared/ProductContainer.tsx
+++ components/shared/ProductContainer.tsx
@@ -1,5 +1,6 @@
-import { ProductType } from '@/lib/constants';
 import React from 'react';
+import Image from 'next/image';
+
 import {
   Carousel,
   CarouselChevronNext,
@@ -8,9 +9,7 @@ import {
   CarouselItem,
 } from '../ui/carousel';
 import { Card, CardContent } from '../ui/card';
-import Image from 'next/image';
-import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
-import { InfoIcon } from 'lucide-react';
+import { ProductType } from '@/lib/constants';
 
 type Props = {
   title: string;
@@ -28,20 +27,20 @@ export default function ProductContainer({
       <div className='bg-primary-500 w-full h-full flex items-center'>
         <div className='wrapper flex flex-col gap-2'>
           <h1 className='text-slate-200 text-3xl sm:text-4xl md:text-5xl 2xl:text-6xl font-bold'>
-            {title}
+            {title.toUpperCase()}
           </h1>
           <p className='text-slate-200 text-md md:text-xl 2xl:text-2xl font-medium tracking-wide'>
-            {description}
+            {description.toUpperCase()}
           </p>
         </div>
       </div>
-      <div className='wrapper'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 md:gap-10 lg:gap-8 xl:gap-10 '>
+      <div className='bg-man-boy'>
+        <div className=' wrapper grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 md:gap-6'>
           {productsData.map((product) => {
-            const { title, color, canBeCustomized, material, images } = product;
+            const { title, color, material, images } = product;
             return (
               <div
-                className='mx-auto border border-1 border-primary-500 rounded-md w-60 sm:w-full'
+                className='mx-auto w-60 sm:w-full'
                 key={`${title} - ${color}`}
               >
                 <div className='w-full relative'>
@@ -53,12 +52,11 @@ export default function ProductContainer({
                     <CarouselContent className='w-fit left-0 right-0'>
                       {images.map((imageSrc, index) => (
                         <CarouselItem className='' key={`${title} - 0${index}`}>
-                          <Card>
+                          <Card className='!rounded-none'>
                             <CardContent className='p-0'>
                               <Image
                                 src={imageSrc}
                                 alt={`${color} ${title} - image - 0${index}`}
-                                className='rounded-md border-b-[1px] border-primary-500 group-hover:border-red-500'
                                 priority
                               />
                             </CardContent>
@@ -71,23 +69,9 @@ export default function ProductContainer({
                       variant='secondary'
                     />
                   </Carousel>
-                  <div className='p-2 lg:py-4 flex justify-between items-center'>
-                    <h1 className='lg:text-lg'>{title}</h1>
-                    <Popover>
-                      <PopoverTrigger>
-                        <InfoIcon size={20} className='stroke-primary-500' />
-                      </PopoverTrigger>
-                      <PopoverContent className='bg-white text-sm w-52'>
-                        <ul>
-                          <li>Color: {color}</li>
-                          <li>Material: {material}</li>
-                          <li>
-                            Can be customized: {canBeCustomized ? 'Yes' : 'No'}
-                          </li>
-                        </ul>
-                      </PopoverContent>
-                    </Popover>
-                  </div>
+                  <h1 className='text-xs uppercase py-2 px-1'>
+                    {color} {title} made of {material}
+                  </h1>
                 </div>
               </div>
             );
