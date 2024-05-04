2f47e95194f2ccf047d35b62329e077f8c0c879a
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-05-04 14:59:10 +0200

    add images and prepare initial products data for men's product

---
+++ public/assets/products/man/index.ts
@@ -0,0 +1,87 @@
+import manBeigeTrousers01 from './man-beige-trousers-01.jpg';
+import manBeigeTrousers02 from './man-beige-trousers-02.jpg';
+import manBeigeTrousers03 from './man-beige-trousers-03.jpg';
+import manBlackHoodie01 from './man-black-hoodie-01.jpg';
+import manBlackHoodie02 from './man-black-hoodie-02.jpg';
+import manBlackHoodie03 from './man-black-hoodie-03.jpg';
+import manBlackHoodie04 from './man-black-hoodie-04.jpg';
+import manBlackTshirt01 from './man-black-tshirt-01.jpg';
+import manBlackTshirt02 from './man-black-tshirt-02.jpg';
+import manBlackTshirt03 from './man-black-tshirt-03.jpg';
+import manBlackUndershirt01 from './man-black-undershirt-01.jpg';
+import manBlackUndershirt02 from './man-black-undershirt-02.jpg';
+import manBlackUndershirt03 from './man-black-undershirt-03.jpg';
+import manBlackUndershirt04 from './man-black-undershirt-04.jpg';
+import manBlueTshirt01 from './man-blue-tshirt-01.jpg';
+import manBlueTshirt02 from './man-blue-tshirt-02.jpg';
+import manBlueTshirt03 from './man-blue-tshirt-03.jpg';
+import manDarkblueHoodie01 from './man-darkblue-hoodie-01.jpg';
+import manDarkblueHoodie02 from './man-darkblue-hoodie-02.jpg';
+import manDarkblueHoodie03 from './man-darkblue-hoodie-03.jpg';
+import manDarkblueTshirt01 from './man-darkblue-tshirt-01.jpg';
+import manDarkblueTshirt02 from './man-darkblue-tshirt-02.jpg';
+import manDarkblueTshirt03 from './man-darkblue-tshirt-03.jpg';
+import manGreenTshirt01 from './man-green-tshirt-01.jpg';
+import manGreenTshirt02 from './man-green-tshirt-02.jpg';
+import manGreenTshirt03 from './man-green-tshirt-03.jpg';
+import manGreenZippedHoodie01 from './man-green-zipped-hoodie-01.jpg';
+import manGreenZippedHoodie02 from './man-green-zipped-hoodie-02.jpg';
+import manGreenZippedHoodie03 from './man-green-zipped-hoodie-03.jpg';
+import manGreenZippedHoodie04 from './man-green-zipped-hoodie-04.jpg';
+import manGreyPoloshirt01 from './man-grey-poloshirt-01.jpg';
+import manGreyPoloshirt02 from './man-grey-poloshirt-02.jpg';
+import manGreyPoloshirt03 from './man-grey-poloshirt-03.jpg';
+import manGreyTshirt01 from './man-grey-tshirt-01.jpg';
+import manGreyTshirt02 from './man-grey-tshirt-02.jpg';
+import manGreyTshirt03 from './man-grey-tshirt-03.jpg';
+import manWhiteTshirt01 from './man-white-tshirt-01.jpg';
+import manWhiteTshirt02 from './man-white-tshirt-02.jpg';
+import manWhiteTshirt03 from './man-white-tshirt-03.jpg';
+import manWhiteUndershirt01 from './man-white-undershirt-01.jpg';
+import manWhiteUndershirt02 from './man-white-undershirt-02.jpg';
+import manWhiteUndershirt03 from './man-white-undershirt-03.jpg';
+
+export {
+  manBeigeTrousers01,
+  manBeigeTrousers02,
+  manBeigeTrousers03,
+  manBlackHoodie01,
+  manBlackHoodie02,
+  manBlackHoodie03,
+  manBlackHoodie04,
+  manBlackTshirt01,
+  manBlackTshirt02,
+  manBlackTshirt03,
+  manBlackUndershirt01,
+  manBlackUndershirt02,
+  manBlackUndershirt03,
+  manBlackUndershirt04,
+  manBlueTshirt01,
+  manBlueTshirt02,
+  manBlueTshirt03,
+  manDarkblueHoodie01,
+  manDarkblueHoodie02,
+  manDarkblueHoodie03,
+  manDarkblueTshirt01,
+  manDarkblueTshirt02,
+  manDarkblueTshirt03,
+  manGreenTshirt01,
+  manGreenTshirt02,
+  manGreenTshirt03,
+  manGreenZippedHoodie01,
+  manGreenZippedHoodie02,
+  manGreenZippedHoodie03,
+  manGreenZippedHoodie04,
+  manGreyPoloshirt01,
+  manGreyPoloshirt02,
+  manGreyPoloshirt03,
+  manGreyTshirt01,
+  manGreyTshirt02,
+  manGreyTshirt03,
+  manWhiteTshirt01,
+  manWhiteTshirt02,
+  manWhiteTshirt03,
+  manWhiteUndershirt01,
+  manWhiteUndershirt02,
+  manWhiteUndershirt03,
+};
