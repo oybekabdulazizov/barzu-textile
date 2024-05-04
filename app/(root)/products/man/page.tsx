d40ff6e5037b70b4445eaafc29cf37a49ed266af
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-05-04 15:18:52 +0200

    add men's products page

---
+++ app/(root)/products/man/page.tsx
@@ -0,0 +1,19 @@
+import { ProductContainer } from '@/components/shared/Product';
+import { productsMan } from '@/lib/constants';
+
+export default function page() {
+  const title = 'For Men';
+  const description = `Step into a world of sophistication and functionality with our men's clothing collection. 
+                      From sharp and casual tailored t-shirts, to trousers and hoodies, our range is designed to cater to every aspect of a man's lifestyle. 
+                      Experience unparalleled comfort and style, whether you're at work, enjoying the outdoors, or relaxing at home. 
+                      Explore our selection to redefine your wardrobe with quality and class.`;
+
+  return (
+    <ProductContainer
+      title={title}
+      description={description}
+      productsData={productsMan}
+      model='man'
+    />
+  );
+}
