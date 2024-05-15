f4bf2c3b80fb89f9dc438c31f6fea00632d41eb6
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-05-15 07:08:54 +0200

    add women's product images and initial data

---
+++ public/assets/products/woman/index.ts
@@ -0,0 +1,181 @@
+import womanBabyblueTurtleneck01 from './woman-babyblue-turtleneck-01.jpg';
+import womanBabyblueTurtleneck02 from './woman-babyblue-turtleneck-02.jpg';
+import womanBabyblueTurtleneck03 from './woman-babyblue-turtleneck-03.jpg';
+import womanBabyblueTurtleneck04 from './woman-babyblue-turtleneck-04.jpg';
+import womanBabyblueTurtleneck05 from './woman-babyblue-turtleneck-05.jpg';
+import womanBlackLongsleeves01 from './woman-black-longsleeves-01.jpg';
+import womanBlackLongsleeves02 from './woman-black-longsleeves-02.jpg';
+import womanBlackLongsleeves03 from './woman-black-longsleeves-03.jpg';
+import womanBlackLongsleeves04 from './woman-black-longsleeves-04.jpg';
+import womanBlackLongsleeves05 from './woman-black-longsleeves-05.jpg';
+import womanBlackLongsleeves06 from './woman-black-longsleeves-06.jpg';
+import womanBlackTshirt01 from './woman-black-tshirt-01.jpg';
+import womanBlackTshirt02 from './woman-black-tshirt-02.jpg';
+import womanBlackTshirt03 from './woman-black-tshirt-03.jpg';
+import womanBlackTshirt04 from './woman-black-tshirt-04.jpg';
+import womanBlueZippedHoodie01 from './woman-blue-zipped-hoodie-01.jpg';
+import womanBlueZippedHoodie02 from './woman-blue-zipped-hoodie-02.jpg';
+import womanBlueZippedHoodie03 from './woman-blue-zipped-hoodie-03.jpg';
+import womanBlueZippedHoodie04 from './woman-blue-zipped-hoodie-04.jpg';
+import womanBlueZippedHoodie05 from './woman-blue-zipped-hoodie-05.jpg';
+import womanGrayBlackOutfit01 from './woman-gray-black-outfit-01.jpg';
+import womanGrayBlackOutfit02 from './woman-gray-black-outfit-02.jpg';
+import womanGrayBlackOutfit03 from './woman-gray-black-outfit-03.jpg';
+import womanGrayBlackOutfit04 from './woman-gray-black-outfit-04.jpg';
+import womanGrayDarkblueOutfit01 from './woman-gray-darkblue-outfit-01.jpg';
+import womanGrayDarkblueOutfit02 from './woman-gray-darkblue-outfit-02.jpg';
+import womanGrayDarkblueOutfit03 from './woman-gray-darkblue-outfit-03.jpg';
+import womanGrayDarkblueOutfit04 from './woman-gray-darkblue-outfit-04.jpg';
+import womanGrayHoodie01 from './woman-gray-hoodie-01.jpg';
+import womanGrayHoodie02 from './woman-gray-hoodie-02.jpg';
+import womanGrayHoodie03 from './woman-gray-hoodie-03.jpg';
+import womanGrayHoodie04 from './woman-gray-hoodie-04.jpg';
+import womanGrayHoodie05 from './woman-gray-hoodie-05.jpg';
+import womanGrayPinkOutfit01 from './woman-gray-pink-outfit-01.jpg';
+import womanGrayPinkOutfit02 from './woman-gray-pink-outfit-02.jpg';
+import womanGrayPinkOutfit03 from './woman-gray-pink-outfit-03.jpg';
+import womanGrayPinkOutfit04 from './woman-gray-pink-outfit-04.jpg';
+import womanGrayTshirt01 from './woman-gray-tshirt-01.jpg';
+import womanGrayTshirt02 from './woman-gray-tshirt-02.jpg';
+import womanGrayTshirt03 from './woman-gray-tshirt-03.jpg';
+import womanGrayTshirt04 from './woman-gray-tshirt-04.jpg';
+import womanGrayTurtleneck01 from './woman-gray-turtleneck-01.jpg';
+import womanGrayTurtleneck02 from './woman-gray-turtleneck-02.jpg';
+import womanGrayTurtleneck03 from './woman-gray-turtleneck-03.jpg';
+import womanGrayTurtleneck04 from './woman-gray-turtleneck-04.jpg';
+import womanGrayTurtleneck05 from './woman-gray-turtleneck-05.jpg';
+import womanGrayYellowOutfit01 from './woman-gray-yellow-outfit-01.jpg';
+import womanGrayYellowOutfit02 from './woman-gray-yellow-outfit-02.jpg';
+import womanGrayYellowOutfit03 from './woman-gray-yellow-outfit-03.jpg';
+import womanGrayYellowOutfit04 from './woman-gray-yellow-outfit-04.jpg';
+import womanGrayZippedHoodie01 from './woman-gray-zipped-hoodie-01.jpg';
+import womanGrayZippedHoodie02 from './woman-gray-zipped-hoodie-02.jpg';
+import womanGrayZippedHoodie03 from './woman-gray-zipped-hoodie-03.jpg';
+import womanGrayZippedHoodie04 from './woman-gray-zipped-hoodie-04.jpg';
+import womanGrayZippedHoodie05 from './woman-gray-zipped-hoodie-05.jpg';
+import womanGreenHoodie01 from './woman-green-hoodie-01.jpg';
+import womanGreenHoodie02 from './woman-green-hoodie-02.jpg';
+import womanGreenHoodie03 from './woman-green-hoodie-03.jpg';
+import womanGreenHoodie04 from './woman-green-hoodie-04.jpg';
+import womanGreenHoodie05 from './woman-green-hoodie-05.jpg';
+import womanPinkPyjama01 from './woman-pink-pyjama-01.jpg';
+import womanPinkPyjama02 from './woman-pink-pyjama-02.jpg';
+import womanPinkPyjama03 from './woman-pink-pyjama-03.jpg';
+import womanPinkPyjama04 from './woman-pink-pyjama-04.jpg';
+import womanPinkPyjama05 from './woman-pink-pyjama-05.jpg';
+import womanSandHoodie01 from './woman-sand-hoodie-01.jpg';
+import womanSandHoodie02 from './woman-sand-hoodie-02.jpg';
+import womanSandHoodie03 from './woman-sand-hoodie-03.jpg';
+import womanSandHoodie04 from './woman-sand-hoodie-04.jpg';
+import womanSandHoodie05 from './woman-sand-hoodie-05.jpg';
+import womanWarmivoryblouse01 from './woman-warmivory-blouse-01.jpg';
+import womanWarmivoryblouse02 from './woman-warmivory-blouse-02.jpg';
+import womanWarmivoryblouse03 from './woman-warmivory-blouse-03.jpg';
+import womanWarmivoryblouse04 from './woman-warmivory-blouse-04.jpg';
+import womanWhitePyjama01 from './woman-white-pyjama-01.jpg';
+import womanWhitePyjama02 from './woman-white-pyjama-02.jpg';
+import womanWhitePyjama03 from './woman-white-pyjama-03.jpg';
+import womanWhitePyjama04 from './woman-white-pyjama-04.jpg';
+import womanYellowLongsleeves01 from './woman-yellow-longsleeves-01.jpg';
+import womanYellowLongsleeves02 from './woman-yellow-longsleeves-02.jpg';
+import womanYellowLongsleeves03 from './woman-yellow-longsleeves-03.jpg';
+import womanYellowLongsleeves04 from './woman-yellow-longsleeves-04.jpg';
+import womanYellowLongsleeves05 from './woman-yellow-longsleeves-05.jpg';
+import womanYellowLongsleeves06 from './woman-yellow-longsleeves-06.jpg';
+import womanYellowZippedHoodie01 from './woman-yellow-zipped-hoodie-01.jpg';
+import womanYellowZippedHoodie02 from './woman-yellow-zipped-hoodie-02.jpg';
+import womanYellowZippedHoodie03 from './woman-yellow-zipped-hoodie-03.jpg';
+import womanYellowZippedHoodie04 from './woman-yellow-zipped-hoodie-04.jpg';
+import womanYellowZippedHoodie05 from './woman-yellow-zipped-hoodie-05.jpg';
+
+export {
+  womanBabyblueTurtleneck01,
+  womanBabyblueTurtleneck02,
+  womanBabyblueTurtleneck03,
+  womanBabyblueTurtleneck04,
+  womanBabyblueTurtleneck05,
+  womanBlackLongsleeves01,
+  womanBlackLongsleeves02,
+  womanBlackLongsleeves03,
+  womanBlackLongsleeves04,
+  womanBlackLongsleeves05,
+  womanBlackLongsleeves06,
+  womanBlackTshirt01,
+  womanBlackTshirt02,
+  womanBlackTshirt03,
+  womanBlackTshirt04,
+  womanBlueZippedHoodie01,
+  womanBlueZippedHoodie02,
+  womanBlueZippedHoodie03,
+  womanBlueZippedHoodie04,
+  womanBlueZippedHoodie05,
+  womanGrayBlackOutfit01,
+  womanGrayBlackOutfit02,
+  womanGrayBlackOutfit03,
+  womanGrayBlackOutfit04,
+  womanGrayDarkblueOutfit01,
+  womanGrayDarkblueOutfit02,
+  womanGrayDarkblueOutfit03,
+  womanGrayDarkblueOutfit04,
+  womanGrayHoodie01,
+  womanGrayHoodie02,
+  womanGrayHoodie03,
+  womanGrayHoodie04,
+  womanGrayHoodie05,
+  womanGrayPinkOutfit01,
+  womanGrayPinkOutfit02,
+  womanGrayPinkOutfit03,
+  womanGrayPinkOutfit04,
+  womanGrayTshirt01,
+  womanGrayTshirt02,
+  womanGrayTshirt03,
+  womanGrayTshirt04,
+  womanGrayTurtleneck01,
+  womanGrayTurtleneck02,
+  womanGrayTurtleneck03,
+  womanGrayTurtleneck04,
+  womanGrayTurtleneck05,
+  womanGrayYellowOutfit01,
+  womanGrayYellowOutfit02,
+  womanGrayYellowOutfit03,
+  womanGrayYellowOutfit04,
+  womanGrayZippedHoodie01,
+  womanGrayZippedHoodie02,
+  womanGrayZippedHoodie03,
+  womanGrayZippedHoodie04,
+  womanGrayZippedHoodie05,
+  womanGreenHoodie01,
+  womanGreenHoodie02,
+  womanGreenHoodie03,
+  womanGreenHoodie04,
+  womanGreenHoodie05,
+  womanPinkPyjama01,
+  womanPinkPyjama02,
+  womanPinkPyjama03,
+  womanPinkPyjama04,
+  womanPinkPyjama05,
+  womanSandHoodie01,
+  womanSandHoodie02,
+  womanSandHoodie03,
+  womanSandHoodie04,
+  womanSandHoodie05,
+  womanWarmivoryblouse01,
+  womanWarmivoryblouse02,
+  womanWarmivoryblouse03,
+  womanWarmivoryblouse04,
+  womanWhitePyjama01,
+  womanWhitePyjama02,
+  womanWhitePyjama03,
+  womanWhitePyjama04,
+  womanYellowLongsleeves01,
+  womanYellowLongsleeves02,
+  womanYellowLongsleeves03,
+  womanYellowLongsleeves04,
+  womanYellowLongsleeves05,
+  womanYellowLongsleeves06,
+  womanYellowZippedHoodie01,
+  womanYellowZippedHoodie02,
+  womanYellowZippedHoodie03,
+  womanYellowZippedHoodie04,
+  womanYellowZippedHoodie05,
+};
